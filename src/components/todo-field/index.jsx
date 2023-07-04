cada7464be117196089f5958a51eab1df98fb72a
Hossein-i <20hossein00@gmail.com>
2023-07-04 17:15:24 +0330

    update todo field

--- src/components/todo-field/index.jsx
+++ src/components/todo-field/index.jsx
@@ -1,5 +1,17 @@
+import { ChevronDownIcon,CheckIcon } from "@heroicons/react/24/outline";
+
 const TodoFieldComponent = () => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <button className="border border-gray-300 p-4">
+        <ChevronDownIcon className="w-5 h-5"/>
+      </button>
+      <input type="text" className="flex-auto border border-gray-300 px-4" placeholder="What need to be done?"/>
+      <button className="border border-gray-300 p-4">
+        <CheckIcon className="w-5 h-5"/>
+      </button>
+    </div>
+  );
 };
 
 export default TodoFieldComponent;
