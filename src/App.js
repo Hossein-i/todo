e28e9cd2cab44308f98e908d14c5d67220cdd6a0
Hossein-i <20hossein00@gmail.com>
2023-07-04 16:37:37 +0330

    add heading-1 to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="pt-20">
+        <h1 className="text-5xl font-extralight text-center">Todo App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
